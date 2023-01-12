367ddb958e9f3675ddd70dcaa9b99e4c045a121b
sookyoungwoo <sookyoung2005@gmail.com>
2023-01-12 17:29:23 +0900

    ADD :: 장소추가 페이지 추가

--- src/pages/AddPlace/index.js
+++ src/pages/AddPlace/index.js
@@ -1,19 +1,69 @@
 import "./style.scss";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
+const Inputs = ({ name, setValue }) => {
+  return (
+    <>
+      <div className="add-top">
+        <p>{name} 추가</p>
+      </div>
+      <div className="add-bottom">
+        <input
+          type="text"
+          onChange={(e) => setValue(e.target.value)}
+          placeholder={`${name}를 입력해주세요.`}
+        />
+      </div>
+    </>
+  );
+};
+const initForm = {
+  place: "",
+  address: "",
+  maximum: "",
+};
+const names = [
+  { key: "place", label: "장소" },
+  { key: "address", label: "주소" },
+  { key: "maximum", label: "최대수용인원" },
+];
 function AddPlace() {
-    return (
-        <div className="add-page">
-            <div className="add-tit">
-                <div className="add-top">
-                    <p>장소추가</p>
-                </div>
-                <div className="add-bottom">
-                    <input type='text' placeholder='도로명 주소를 입력해주세요.'/>
-                </div>
-            </div>
-        </div>
-    );
+  const [pageIndex, setPageIndex] = useState(0);
+  const [form, setForm] = useState();
+  console.log("form : ", form);
+  
+  return (
+    <div className="add-page">
+      <div className="add-tit">
+        <Inputs
+          name={names[pageIndex].label}
+          setValue={(v) => {
+            setForm((s) => {
+              return {
+                ...s,
+                [names[pageIndex].key]: v,
+              };
+            });
+          }}
+        />
+        <button
+          className="add-bottom-button"
+          onClick={() => {
+            if (names.length - 1 > pageIndex) {
+              setPageIndex((s) => s + 1);
+            } else {
+              console.log("send data : ", form);
+              console.log("move homepage");
+              setForm(initForm);
+            }
+          }}
+        >
+          완료
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default AddPlace;
